4d02fe3e402d7879727fcf0f929fc215638a4025
HuSh-s <semihbay17@gmail.com>
2025-06-03 17:39:16 +0300

    Product_List
    Product List started

---
+++ ProductList/Product_List/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Product_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataZone = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataZone)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataZone
+            // 
+            this.DataZone.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DataZone.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.DataZone.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataZone.Location = new System.Drawing.Point(360, 33);
+            this.DataZone.Name = "DataZone";
+            this.DataZone.RowHeadersWidth = 51;
+            this.DataZone.RowTemplate.Height = 24;
+            this.DataZone.Size = new System.Drawing.Size(405, 478);
+            this.DataZone.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 548);
+            this.Controls.Add(this.DataZone);
+            this.Name = "Form1";
+            this.Text = "Product List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataZone)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataZone;
+    }
+}
+
---
+++ ProductList/Product_List/Product_Dal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_List
+{
+    public class Product_Dal
+    {
+        public List<Products> Print()
+        {
+            SqlConnection connection = new SqlConnection(@"server = (localdb)\mssqllocaldb; initial catalog = Products; integrated security = true");
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            SqlCommand command = new SqlCommand("Select *from Products", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            List<Products> products = new List<Products>();
+
+            while (reader.Read()) 
+            {
+                Products product = new Products
+                {
+                    Id = Convert.ToInt32(reader["ID"]),
+                    Name = Convert.ToString(reader["Name"]),
+                    Stock = Convert.ToInt32(reader["Stock"]),
+                    Price = Convert.ToInt32(reader["Price"]),
+                };
+
+                products.Add(product);
+            }
+
+            reader.Close();
+            connection.Close();
+            return products;
+
+
+        }
+
+
+    }
+}
