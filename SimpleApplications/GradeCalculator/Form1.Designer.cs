a07d73ac5ac0f3908d26674cba0ff2c6dc22b08c
HuSh-s <semihbay17@gmail.com>
2025-06-04 18:59:24 +0300

    Finished basic GradeCalculator app
    This Windows Forms application allows the user to enter three exam scores and a custom passing grade. It calculates the average score and displays whether the student has passed or failed based on the entered passing grade. The app also includes a "Clear" button to reset all fields.

---
+++ SimpleApplications/GradeCalculator/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace GradeCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtExam1 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtExam2 = new System.Windows.Forms.TextBox();
+            this.txtExam3 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Exam 1";
+            // 
+            // txtExam1
+            // 
+            this.txtExam1.Location = new System.Drawing.Point(110, 46);
+            this.txtExam1.Name = "txtExam1";
+            this.txtExam1.Size = new System.Drawing.Size(118, 22);
+            this.txtExam1.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(110, 214);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(118, 36);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Exam 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Exam 3";
+            // 
+            // txtExam2
+            // 
+            this.txtExam2.Location = new System.Drawing.Point(110, 110);
+            this.txtExam2.Name = "txtExam2";
+            this.txtExam2.Size = new System.Drawing.Size(118, 22);
+            this.txtExam2.TabIndex = 5;
+            // 
+            // txtExam3
+            // 
+            this.txtExam3.Location = new System.Drawing.Point(110, 167);
+            this.txtExam3.Name = "txtExam3";
+            this.txtExam3.Size = new System.Drawing.Size(118, 22);
+            this.txtExam3.TabIndex = 6;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(110, 392);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(118, 22);
+            this.txtResult.TabIndex = 7;
+            // 
+            // btnClear
+            // 
+            this.btnClear.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnClear.Location = new System.Drawing.Point(110, 268);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(118, 36);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Result";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(120, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Passing Grade";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(110, 335);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(118, 22);
+            this.txtPass.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(343, 437);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtExam3);
+            this.Controls.Add(this.txtExam2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtExam1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Grade Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtExam1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtExam2;
+        private System.Windows.Forms.TextBox txtExam3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPass;
+    }
+}
+
---
+++ SimpleApplications/GradeCalculator/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int exam1, exam2, exam3, average, passsing_grade;
+            exam1 = Convert.ToInt32(txtExam1.Text);
+            exam2 = Convert.ToInt32(txtExam2.Text);
+            exam3 = Convert.ToInt32(txtExam3.Text);
+            passsing_grade = Convert.ToInt32(txtPass.Text);
+
+            average = (exam1 + exam2 + exam3) / 3;
+
+            if (average <= passsing_grade)
+            {
+                txtResult.Text = average.ToString();
+                txtResult.BackColor = Color.Red;
+                txtResult.ForeColor = Color.White;
+            }
+            else
+            {
+                txtResult.Text = average.ToString();
+                txtResult.BackColor = Color.Green;
+                txtResult.ForeColor = Color.White;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtExam1.Text = "";
+            txtExam2.Text = "";
+            txtExam3.Text = "";
+            txtResult.Text = "";
+            txtPass.Text = "";
+            txtResult.BackColor = Color.White;
+            txtResult.ForeColor = Color.Black;
+        }
+    }
+}
